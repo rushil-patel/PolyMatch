b86c6d24a62babd99b4e11d67afd19b0feaf8e9e
Chris Yerina <cyerina@gmail.com>
2017-06-11 19:32:03 -0700

    implemented chip feature on UI

--- public/Preferences/preferencesController.js
+++ public/Preferences/preferencesController.js
@@ -8,14 +8,36 @@ app.controller('preferencesController',
     $scope.majorList = majorList;
     $scope.majorSearch = "";
 
+    //Hobby Chips
+    $scope.selectedItem = null;
+    $scope.searchText = null;
+    $scope.hobbyList = [
+      {
+        id: 1,
+        name: "running"
+      },
+      {
+        id: 2,
+        name: "swimming"
+      },
+      {
+        id: 3,
+        name: "swearing"
+      },
+      {
+        id: 4,
+        name: "hiking"
+      }];
+    $scope.userHobbies = [];
+
     $scope.gradesRatio = 50;
     $scope.gradesRatioUpdate = function() {
       $scope.socialRatio = 100 - $scope.gradesRatio;
-   }
+   };
     $scope.socialRatio = 50;
     $scope.socialRatioUpdate = function() {
       $scope.gradesRatio = 100 - $scope.socialRatio;
-   }
+   };
 
    $scope.quite = false;
    $scope.greekLife = false;
@@ -36,7 +58,7 @@ app.controller('preferencesController',
           .indexOf($scope.dormSearch.toLowerCase()) >= 0
       })
       return $q.when(filteredDorms)
-   }
+   };
 
    $scope.queryMajor = function(search) {
       var filteredMajors = majorList.filter(function(item) {
@@ -44,6 +66,37 @@ app.controller('preferencesController',
           .indexOf($scope.majorSearch.toLowerCase()) >= 0
       })
       return $q.when(filteredMajors)
-   }
+   };
+
+   $scope.transformChip = function(chip) {
+      var createChip;
+
+      if (angular.isObject(chip))
+        return chip;
+      else {
+        createChip = {id: -1, name: chip};
+        //$scope.userHobbies.push(createChip);
+        return createChip;
+      }
+   };
+
+   $scope.queryHobby = function(search) {
+      var results = search ? this.hobbyList.filter(this.createFilterFor(search)) : [];
+      return results;
+   };
+
+   $scope.createFilterFor = function(search) {
+      var lowercaseQuery = angular.lowercase(search);
+      return function filterFunc(hobby) {
+        return hobby.name.toLowerCase().indexOf(lowercaseQuery) === 0;
+      }
+   };
+
+   $scope.exitChip = function(chip, index) {
+      console.log(chip.name);
+      console.log(chip.id);
+      console.log(index);
+      console.log("exit chip");
+   };
 
 }]);
