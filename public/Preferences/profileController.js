08484eba9b1667e1a9e7ffff30b06ad75a78ee80
Rushil Patel <rushil9@gmail.com>
2017-06-12 15:08:54 -0700

    made the user info editable from profile

--- public/Preferences/profileController.js
+++ public/Preferences/profileController.js
@@ -3,6 +3,22 @@ app.controller('profileController',
    function($scope, $state, $http, login, nDlg, user, preferences) {
    	$scope.user = user[0];
    	$scope.preferences = preferences[0];
+      $scope.imageUpload = function(event) {
+         var files = event.target.files;
+         var file = files[files.length-1];
+         $scope.file = file;
+         var reader = new FileReader();
+         reader.onload = $scope.imageIsLoaded;
+         reader.readAsDataURL(file);
+      }
 
+      $scope.imageIsLoaded = function(e) {
+         $scope.$apply(function() {
+            $scope.profileImageData = e.target.result;
+         })
+      }
 
-}]);
+      $scope.setGender = function(gender) {
+         $scope.user.gender = gender;
+      }
+ }]);
