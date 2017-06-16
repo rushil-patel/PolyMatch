f4667fdfe4ba844c37275191949b887fd883b46a
sshaul <sshaul@calpoly.edu>
2017-06-16 07:35:54 -0700

    fixed spacing

--- Routes/Matches/Matches.js
+++ Routes/Matches/Matches.js
@@ -5,103 +5,106 @@ var async = require('async');
 var mysql = require('mysql');
 
 router.get('/:usrId/Matches', function(req, res) {
-	var query = req.query;
-	var keys = Object.keys(query);
-	var usrId = req.params.usrId;
-	var vld = req.validator;
-	var cnn = req.cnn;
+   var query = req.query;
+   var keys = Object.keys(query);
+   var usrId = req.params.usrId;
+   var vld = req.validator;
+   var cnn = req.cnn;
 
-	async.waterfall([
-	function(cb) {
-		if (vld.checkPrsOK(usrId, cb)) {
-			// Create where clause
-			pairs = [];
-			keys.forEach(function(key) {
-				pairs.push(key + " = " + query[key]);
-			});
+   async.waterfall([
+   function(cb) {
+      if (vld.checkPrsOK(usrId, cb)) {
+         // Create where clause
+         pairs = [];
+         keys.forEach(function(key) {
+            pairs.push(key + " = " + query[key]);
+         });
 
-			var where = "";
-			if (pairs.length) {
-				where = ' where ' + pairs.join(" and ");
-			}
+         var where = "";
+         if (pairs.length) {
+            where = ' where ' + pairs.join(" and ");
+         }
 
-			cnn.chkQry('select * from (select id as matchId, oldPerson as usr, score, saved, archived, notes' +
-			    ' from Matches where newPerson = ?) as S JOIN (select * from User)' +
-			    ' as U ON S.usr = U.id JOIN (select * from Preferences) as P ON U.id = P.userId'
-			    + where + ' order by score desc',
-				[usrId], cb);
-		}
-	},
-	function(result, fields, cb) {
-		res.status(200).json(result);
-		cb();
-	}],
-	function(err) {
-		cnn.release();
-	});
+         cnn.chkQry('select * from (select id as matchId, oldPerson as' +
+            ' usr, score, saved, archived, notes from Matches where' +
+            ' newPerson = ?) as S JOIN (select * from User)' +
+             ' as U ON S.usr = U.id JOIN (select * from Preferences)' +
+             ' as P ON U.id = P.userId'
+             + where + ' order by score desc',
+            [usrId], cb);
+      }
+   },
+   function(result, fields, cb) {
+      res.status(200).json(result);
+      cb();
+   }],
+   function(err) {
+      cnn.release();
+   });
 });
 
 
 router.put('/:usrId/Matches/:mId', function(req, res) {
-	var vld = req.validator;
-	var cnn = req.cnn;
-	var mId = req.params.mId;
-	var usrId = req.params.usrId;
-	var body = req.body;
+   var vld = req.validator;
+   var cnn = req.cnn;
+   var mId = req.params.mId;
+   var usrId = req.params.usrId;
+   var body = req.body;
 
-	console.log(mId);
+   console.log(mId);
 
-	async.waterfall([
-	function(cb) {
-		if (vld.checkPrsOK(usrId, cb) &&
-			vld.check(!(body.saved && body.archived),
-			 Tags.badValue, ['saved', 'archived'], cb)) {
+   async.waterfall([
+   function(cb) {
+      if (vld.checkPrsOK(usrId, cb) &&
+         vld.check(!(body.saved && body.archived),
+          Tags.badValue, ['saved', 'archived'], cb)) {
          cnn.chkQry('select * from Matches where id = ? and newPerson = ?',
           [mId, usrId], cb)
-		}
-	},
+      }
+   },
    function(matchResult, fields, cb) {
       if (vld.check(matchResult.length, Tags.notFound, null, cb)) {
          cnn.chkQry('update Matches set ? where id = ?', [body, mId], cb);
       };
    }],
-	function(err) {
-		if (!err) {
-			res.status(200).end();
-		}
-		cnn.release();
-	});
+   function(err) {
+      if (!err) {
+         res.status(200).end();
+      }
+      cnn.release();
+   });
 });
 
 router.get('/:usrId/Matches/:mId', function(req, res) {
-	var vld = req.validator;
-	var cnn = req.cnn;
-	var mId = req.params.mId;
-	var usrId = req.params.usrId;
+   var vld = req.validator;
+   var cnn = req.cnn;
+   var mId = req.params.mId;
+   var usrId = req.params.usrId;
 
-	async.waterfall([
-	function(cb) {
-		if (vld.checkPrsOK(usrId, cb)) {
-			cnn.chkQry('select * from Matches where id = ? and ' +
-			 'newPerson = ?', [mId, usrId], cb);
-		}
-	},
-	function(match, fields, cb) {
-		if (vld.check(match.length, Tags.notFound, null, cb)) {
-			cnn.chkQry('select M.id, score, firstName, lastName, email, ' +
-			 ' gender, age, introduction, pictureUrl, saved, archived, notes from Matches M JOIN User' +
-			 ' U ON M.oldPerson = U.id where M.id = ?', [mId], cb);
-		}
-	},
-	function(matchInfo, fields, cb) {
-		matchInfo[0].saved = !!matchInfo[0].saved;
-		matchInfo[0].archived = !!matchInfo[0].archived;
-		res.status(200).json(matchInfo);
-		cb();
-	}],
-	function(err) {
-		cnn.release();
-	});
+   async.waterfall([
+   function(cb) {
+      if (vld.checkPrsOK(usrId, cb)) {
+         cnn.chkQry('select * from Matches where id = ? and ' +
+          'newPerson = ?', [mId, usrId], cb);
+      }
+   },
+   function(match, fields, cb) {
+      if (vld.check(match.length, Tags.notFound, null, cb)) {
+         cnn.chkQry('select M.id, score, firstName, lastName, email, ' +
+          'gender, age, introduction, pictureUrl, saved, archived, ' +
+          'notes from Matches M JOIN User ' +
+          'U ON M.oldPerson = U.id where M.id = ?', [mId], cb);
+      }
+   },
+   function(matchInfo, fields, cb) {
+      matchInfo[0].saved = !!matchInfo[0].saved;
+      matchInfo[0].archived = !!matchInfo[0].archived;
+      res.status(200).json(matchInfo);
+      cb();
+   }],
+   function(err) {
+      cnn.release();
+   });
 });
 
 
--- public/Matches/matchesController.js
+++ public/Matches/matchesController.js
@@ -1,170 +1,168 @@
 app.controller('matchesController',
  ['$scope', '$state', '$http', 'login', 'notifyDlg', 'matches', 'dormList', 
   'majorList',
-  function($scope, $state, $http, login, nDlg, matches, dormList, majorList) {
-  $scope.title = $state.current.data.title;
-  $scope.extraQuery = $state.current.data.extraQuery;
-  $scope.filters = ('firstName lastName major dormName gradeRatio ' +
-     'quiet greekLife smoking drinking wakeTime sleepTime ' +
-     'cleanliness').split(' ').map(function(f) {
-    return {filter: f};
-  });
+   function($scope, $state, $http, login, nDlg, matches, dormList, majorList) {
+      $scope.title = $state.current.data.title;
+      $scope.extraQuery = $state.current.data.extraQuery;
+      $scope.filters = ('firstName lastName major dormName gradeRatio ' +
+       'quiet greekLife smoking drinking wakeTime sleepTime ' +
+       'cleanliness').split(' ').map(function(f) {
+         return {filter: f};
+   });
 
-  $scope.matches = matches;
+   $scope.matches = matches;
 
-  $scope.currentFilters = [];
+   $scope.currentFilters = [];
 
-  $scope.saveMatch = function(matchId, boolean) {
+   $scope.saveMatch = function(matchId, boolean) {
 
-     login.getUser().then(function(user) {
-        $http.put('Users/' + user.id + '/Matches/' + matchId, 
-         {'saved': boolean}).then(function(response) {
-           $state.reload();
-         }).catch(function(err) {
-           console.log('not saved');
-         });
-     });
+      login.getUser().then(function(user) {
+         $http.put('Users/' + user.id + '/Matches/' + matchId, 
+          {'saved': boolean}).then(function(response) {
+            $state.reload();
+          }).catch(function(err) {
+            console.log('not saved');
+          });
+      });
 
      
   };
 
-  $scope.hideMatch = function(matchId, boolean) {
+   $scope.hideMatch = function(matchId, boolean) {
 
-     login.getUser().then(function(user) {
-        $http.put('Users/' + user.id + '/Matches/' + matchId, 
-         {'archived': boolean}).then(function(response) {
-           $state.reload();
-         }).catch(function(err) {
-           console.log('not hidden');
-         });
-     });
-  };
+      login.getUser().then(function(user) {
+         $http.put('Users/' + user.id + '/Matches/' + matchId, 
+          {'archived': boolean}).then(function(response) {
+             $state.reload();
+          }).catch(function(err) {
+             console.log('not hidden');
+          });
+      });
+   };
 
-  $scope.addNote = function(index) {
-     matches[index].newNote = true;
-  };
+   $scope.addNote = function(index) {
+      matches[index].newNote = true;
+   };
 
-  $scope.submitNote = function(index, note) {
-     matches[index].newNote = false;
-     login.getUser().then(function(user) {
-        $http.put('Users/' + user.id + '/Matches/' + matches[index].matchId,
+   $scope.submitNote = function(index, note) {
+      matches[index].newNote = false;
+      login.getUser().then(function(user) {
+         $http.put('Users/' + user.id + '/Matches/' + matches[index].matchId,
          {'notes': note}).then(function(response) {
-           $state.reload();
+            $state.reload();
          });
-     });
-  };
+      });
+   };
 
-  $scope.filter = function() {
-     $scope.currentFilters.push($scope.filterApplied);
-     var length = Object.keys($scope.currentFilters).length;
-     var query;
-     login.getUser().then(function(response){
-        query = "/Users/" + response.id + 
-         "/Matches?" + $scope.extraQuery;
+   $scope.filter = function() {
+      $scope.currentFilters.push($scope.filterApplied);
+      var length = Object.keys($scope.currentFilters).length;
+      var query;
+      login.getUser().then(function(response){
+         query = "/Users/" + response.id + 
+          "/Matches?" + $scope.extraQuery;
 
         for (var filter in $scope.currentFilters) {
-           query += " && " + $scope.currentFilters[filter].name + "=";
-           if ($scope.currentFilters[filter].name === "firstName" ||
-            $scope.currentFilters[filter].name === "lastName") {
-              query += "'";
-           }
-           var value = $scope.currentFilters[filter].value;
-           if ($scope.currentFilters[filter].name === "dormName") {
-              for (var dorm in dormList) {
-                 if (dormList[dorm].name === $scope.currentFilters[filter].value) {
-                    value = dormList[dorm].id;
-                    break;
-                 }
-              }
-              if (value === $scope.currentFilters[filter].value) {
+            query += " && " + $scope.currentFilters[filter].name + "=";
+            if ($scope.currentFilters[filter].name === "firstName" ||
+             $scope.currentFilters[filter].name === "lastName") {
+               query += "'";
+            }
+            var value = $scope.currentFilters[filter].value;
+            if ($scope.currentFilters[filter].name === "dormName") {
+               for (var dorm in dormList) {
+                  if (dormList[dorm].name === 
+                   $scope.currentFilters[filter].value) {
+                     value = dormList[dorm].id;
+                     break;
+                  }
+               }
+               if (value === $scope.currentFilters[filter].value) {
                  value = 0;
-              }
-           }
-           else if ($scope.currentFilters[filter].name === "major") {
-              for (var major in majorList) {
-                 if (majorList[major].name === $scope.currentFilters[filter].value) {
-                    value = majorList[major].id;
-                    break;
-                 }
-              }
-              if (value === $scope.currentFilters[filter].value) {
-                 value = 0;
-              }
-           }
+               }
+            }
+            else if ($scope.currentFilters[filter].name === "major") {
+               for (var major in majorList) {
+                  if (majorList[major].name === $scope.currentFilters[filter].value) {
+                     value = majorList[major].id;
+                     break;
+                  }
+               }
+               if (value === $scope.currentFilters[filter].value) {
+                  value = 0;
+               }
+            }
            
-           query += value;
-           if ($scope.currentFilters[filter].name === "firstName" ||
-            $scope.currentFilters[filter].name === "lastName") {
-              query += "'";
-           }
-        }
-        console.log(query);
+            query += value;
+            if ($scope.currentFilters[filter].name === "firstName" ||
+             $scope.currentFilters[filter].name === "lastName") {
+               query += "'";
+            }
+         }
 
-        $http.get(query)
-           .then(function(response) {
-              $scope.matches = response.data;
-           });
-        $scope.filterApplied = {};
-     });
-  }
+         $http.get(query)
+            .then(function(response) {
+               $scope.matches = response.data;
+            });
+         $scope.filterApplied = {};
+      });
+   }
 
-  $scope.removeFilter = function(filter) {
-     var index = $scope.currentFilters.indexOf(filter);
-     var length = Object.keys($scope.currentFilters).length;
-     console.log(index);
-     var first = $scope.currentFilters.splice(0, index);
-     var last = $scope.currentFilters.splice(index + 1, length);
-     $scope.currentFilters = first.concat(last);
-     console.log($scope.currentFilters);
-     length = Object.keys($scope.currentFilters).length;
+   $scope.removeFilter = function(filter) {
+      var index = $scope.currentFilters.indexOf(filter);
+      var length = Object.keys($scope.currentFilters).length;
+      var first = $scope.currentFilters.splice(0, index);
+      var last = $scope.currentFilters.splice(index + 1, length);
+      $scope.currentFilters = first.concat(last);
+      console.log($scope.currentFilters);
+      length = Object.keys($scope.currentFilters).length;
 
-     login.getUser().then(function(response){
-        query = "/Users/" + response.id + 
-         "/Matches?" + $scope.extraQuery;
+      login.getUser().then(function(response){
+         query = "/Users/" + response.id + 
+          "/Matches?" + $scope.extraQuery;
 
-        for (var filter in $scope.currentFilters) {
-           query += " && " + $scope.currentFilters[filter].name + "=";
-           if ($scope.currentFilters[filter].name === "firstName" ||
-            $scope.currentFilters[filter].name === "lastName") {
-              query += "'";
-           }
-           var value = $scope.currentFilters[filter].value;
-           if ($scope.currentFilters[filter].name === "dormName") {
-              for (var dorm in dormList) {
-                 if (dormList[dorm].name === $scope.currentFilters[filter].value) {
-                    value = dormList[dorm].id;
-                    break;
-                 }
-              }
-              if (value === $scope.currentFilters[filter].value) {
-                 value = 0;
-              }
-           }
-           else if ($scope.currentFilters[filter].name === "major") {
-              for (var major in majorList) {
-                 if (majorList[major].name === $scope.currentFilters[filter].value) {
-                    value = majorList[major].id;
-                    break;
-                 }
-              }
-              if (value === $scope.currentFilters[filter].value) {
-                 value = 0;
-              }
-           }
-           query += value;
-           if ($scope.currentFilters[filter].name === "firstName" ||
-            $scope.currentFilters[filter].name === "lastName") {
-              query += "'";
-           }
-        }
-        console.log(query);
+         for (var filter in $scope.currentFilters) {
+            query += " && " + $scope.currentFilters[filter].name + "=";
+            if ($scope.currentFilters[filter].name === "firstName" ||
+             $scope.currentFilters[filter].name === "lastName") {
+               query += "'";
+            }
+            var value = $scope.currentFilters[filter].value;
+            if ($scope.currentFilters[filter].name === "dormName") {
+               for (var dorm in dormList) {
+                  if (dormList[dorm].name === $scope.currentFilters[filter].value) {
+                     value = dormList[dorm].id;
+                     break;
+                  }
+               }
+               if (value === $scope.currentFilters[filter].value) {
+                  value = 0;
+               }
+            }
+            else if ($scope.currentFilters[filter].name === "major") {
+               for (var major in majorList) {
+                  if (majorList[major].name === $scope.currentFilters[filter].value) {
+                     value = majorList[major].id;
+                     break;
+                  }
+               }
+               if (value === $scope.currentFilters[filter].value) {
+                  value = 0;
+               }
+            }
+            query += value;
+            if ($scope.currentFilters[filter].name === "firstName" ||
+             $scope.currentFilters[filter].name === "lastName") {
+               query += "'";
+            }
+         }
 
-        $http.get(query)
-           .then(function(response) {
-              $scope.matches = response.data;
-           });
-        $scope.filterApplied = {};
-     });
-  }
+         $http.get(query)
+            .then(function(response) {
+               $scope.matches = response.data;
+            });
+         $scope.filterApplied = {};
+      });
+   }
 
 }]);
